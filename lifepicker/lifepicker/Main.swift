65198c4573cf3dff4476cd285ac52138cc5edc77
Ali Al Abdullah <38346718+arcticxo@users.noreply.github.com>
2024-02-13 01:04:13 +0000

    Initial push

---
+++ lifepicker/lifepicker/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  lifepicker
+//
+//  Created by Ali Al Abdullah on 04/02/2024.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
